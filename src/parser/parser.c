06243a2d5bb427f2efc3621f146cb5b647d93692
Didac Guillem <didacguillem42@hotmail.com>
2025-07-30 23:45:19 +0200

    docs: add detailed parser documentation matching lexer style

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,20 @@
 /*   By: didguill <didguill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 23:26:13 by didguill          #+#    #+#             */
-/*   Updated: 2025/07/30 23:13:50 by didguill         ###   ########.fr       */
+/*   Updated: 2025/07/30 23:44:24 by didguill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ************************************************************************** */
+/*                                                                            */
+/*   SYNTAX ANALYZER (PARSER)                                                 */
+/*                                                                            */
+/*   The parser processes tokens from the lexer and builds a command          */
+/*   structure representing the shell input. It checks syntax and organizes   */
+/*   commands, pipes, and redirections for execution.                         */
+/*                                                                            */
+/*    Input:  ["ls", "-la", "|", "grep", ".txt", ">", "output"]               */
+/*    Output: Pipeline: Command1("ls -la") | Command2("grep .txt > output")   */
 /*                                                                            */
 /* ************************************************************************** */
 
