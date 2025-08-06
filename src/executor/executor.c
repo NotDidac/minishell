414e12fbbe11cec880ca1228a897b3d57fd6fbce
Didac Guillem <didacguillem42@hotmail.com>
2025-08-06 16:19:09 +0200

    feature: all memory leaks handled correctly

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -6,11 +6,12 @@
 /*   By: didguill <didguill@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 23:31:40 by didguill          #+#    #+#             */
-/*   Updated: 2025/08/04 11:06:34 by didguill         ###   ########.fr       */
+/*   Updated: 2025/08/06 16:18:17 by didguill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser/command_list.h"
+#include "free/clear_commands.h"
 #include "colors.h"
 
 #include <stdio.h>
@@ -22,5 +23,6 @@ void	executor(t_command *commands)
 		printf("\nNo commands to execute.\n");
 		return ;
 	}
+	clear_commands(commands);
 	printf(BLUE "[Executor] " RESET "Executing commands...\n");
 }
